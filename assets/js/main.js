e938d9c3c279a751b8d0aeb5a9de3b82e3e0b66e
Assis Araujo <assissousa207@gmail.com>
2022-03-02 21:30:14 -0400

    Responsivo

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,3 +124,14 @@ themeButton.addEventListener('click', () => {
 })
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    durantion: 2500,
+    delay: 400,
+    // reset: true,
+})
+
+sr.reveal(`.home__data`)
+sr.reveal(`.home__handle`, { delay: 700 })
+sr.reveal(`.home__social, .home__scroll`, { delay: 900, origin: 'bottom' })
